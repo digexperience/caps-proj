47db90032ab6ff02b34dd9b9dedd40e8dbefd8a0
digexperience <jolly0903992@gmail.com>
2024-08-22 04:07:21 +0800

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,19 +3,27 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-12 col-lg-6 col-xl-9 col-md-6 col-sm-12">
+        <span class="text">
+            <strong>
+            <i>LO-KI</i> <br />LABORATORY
+            LOCK <br />KEY MANAGEMENT<br />
+            SYSTEM
+            </strong>
+        </span>
+        </div>
+        <div class="col-12 col-lg-6 col-xl-3 col-md-6 col-sm-12">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
+                <!-- <div class="card-logo">
+                </div> -->
+                <div class="card-header" >{{ __('LOGIN') }}</div>
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            <div class="col-md-12">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus  placeholder="Enter your email address">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -23,14 +31,31 @@
                                     </span>
                                 @enderror
                             </div>
-                        </div>
+                        </div> 
 
                         <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                            <div class="col-md-12">
+                                <div class="password-wrapper">
+                                    <input id="password-field" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Enter your password">
+                                    <span id="sideicon" class="mdi mdi-eye-off-outline field-icon" onclick="togglePassword()"></span>
+                                </div>
+                                <script>
+                                    function togglePassword() {
+                                        var passwordField = document.getElementById("password-field");
+                                        var toggleIcon = document.getElementById("sideicon");
 
+                                        if (passwordField.type === "password") {
+                                            passwordField.type = "text";
+                                            toggleIcon.classList.remove("mdi-eye-off-outline");
+                                            toggleIcon.classList.add("mdi-eye-outline");
+                                        } else {
+                                            passwordField.type = "password";
+                                            toggleIcon.classList.remove("mdi-eye-outline");
+                                            toggleIcon.classList.add("mdi-eye-off-outline");
+                                        }
+                                    }
+                                </script>
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -39,21 +64,19 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
+                        <!-- <div class="row mb-3"> -->
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
                                         {{ __('Remember Me') }}
                                     </label>
-                                </div>
                             </div>
                         </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                        <!-- <div class="row mb-0"> -->
+                            <!-- <div class="col-md-8 offset-md-4"> -->
+                                <button type="submit" class="btn">
                                     {{ __('Login') }}
                                 </button>
 
@@ -69,5 +92,8 @@
             </div>
         </div>
     </div>
+    
 </div>
+
+
 @endsection
